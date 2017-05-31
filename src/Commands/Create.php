e4086fb70436dd1ae64547032fd46366167e36a4
David Seidl <david.seidl@towa.at>
2017-05-31 15:25:55 +0200

    provision vagrant after creation

--- src/Commands/Create.php
+++ src/Commands/Create.php
@@ -1,6 +1,8 @@
 <?php
 namespace Towa\Setup\Commands;
 
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 use Towa\Setup\Command;
 use Towa\Setup\Interfaces\CommandInterface;
 use Towa\Setup\Utilities\YamlParser;
@@ -15,9 +17,18 @@ class Create extends Command implements CommandInterface
         try {
             $this->saveSiteToConfig($siteName, $site);
         } catch (\Exception $e) {
-            $this->climate->error('Meh... failed to update vvv-config.yml');
-            $this->climate->error($e->getMessage());
+            self::$climate->error('Meh... failed to update vvv-config.yml');
+            self::$climate->error($e->getMessage());
         }
+
+        try {
+            $this->provisionSite($siteName);
+        } catch (ProcessFailedException $e) {
+            self::$climate->error("Meh... failed to provision site");
+            self::$climate->error($e->getMessage());
+        }
+
+        $this->notifyOnSuccess($siteName);
     }
 
     private function saveSiteToConfig($siteName, $site)
@@ -59,5 +70,22 @@ class Create extends Command implements CommandInterface
             'nginx_upstream' => $phpVersion,
             'hosts' => $host,
         ];
+    }
+
+    private function provisionSite($siteName)
+    {
+        $vvv = get_config('path');
+        $provision = new Process("cd {$vvv} && vagrant provision --provision-with site-{$siteName}");
+
+        $provision->setTimeout(0)->mustRun(function($type, $buffer) {
+            echo $buffer;
+        });
+    }
+
+    private function notifyOnSuccess($siteName)
+    {
+        self::$climate->info("Site: {$siteName}.dev");
+        self::$climate->info("User: towa_admin");
+        self::$climate->info("Password: dev");
     }
 }
